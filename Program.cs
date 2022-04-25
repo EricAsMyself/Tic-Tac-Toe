45712ef37206c34c985191b15a97070027ac863d
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2022-04-25 11:12:27 -0600

    Added int converter

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+string user_number = "5";
+int int_user_number = 0;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("hi");
+
+user_number = Console.ReadLine();
+int_user_number = Convert.ToInt32(user_number);
+Console.WriteLine(user_number);
